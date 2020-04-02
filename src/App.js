7e29b74c7b24e5a7b29f9e396e1a182c57707510
AlfalahAnugara <alfalah190299@gmail.com>
2020-04-02 08:23:35 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './statefull/Navigation';
+import Keyboardpage from './statefull/Keyboardpage';
+import Headsetpage from './statefull/Headsetpage';
+import Mousepage from './statefull/Mousepage';
+import Homepage from './statefull/Homepage';
+// import Content from './container/Content';
+// import Provinsi from './container/Provinsi';
+// import Footer from './container/Footer';
+// import Hotline from './container/Hotline'/;
+// import TableProvinsi from './components/ListProvinsi';
 import './App.css';
+import { BrowserRouter as Router,Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  return(
+    <div>
+    <Router>
+      <Switch>
+        <Route exact path="/"><HomePage/></Route>
+        <Route path="/headset"><HeadsetPage/></Route>
+        <Route path="/keyboard"><KeyboardPage/></Route>
+        <Route path="/mouse"><MousePage/></Route>
+      </Switch>
+    </Router>
     </div>
-  );
+  )
 }
+const HomePage = () => {
+  return(
+    <div className="container-fluid">
+      <Navigation/>
+      <Homepage/>
+      {/* <Content/> */}
+      {/* <Footer/> */}
+    </div>
+  )
+}
+function HeadsetPage(){
+  return(
+    <div className="container-fluid">
+      <Navigation/>
+      <Headsetpage/>
+    </div>
 
-export default App;
+  )
+}
+function KeyboardPage(){
+  return(
+    <div className="container-fluid">
+      <Navigation/>
+      <Keyboardpage/>
+    </div>
+  )
+}
+function MousePage(){
+  return(
+    <div className="container-fluid">
+      <Navigation/>
+      <Mousepage/>
+      
+    </div>
+  )
+}
+// const Prov = () => {
+//   return(
+//     <div className="container dark">
+//       <Navbar/>
+//       <Provinsi/>
+//       <Footer/>
+//     </div>
+//   )
+// }
+// const Hot = () => {
+//   return(
+//     <div className="container dark">
+//       <Navbar/>
+//       <Hotline/>
+//       <Footer/>
+//     </div>
+//   )
+// }
+// const Contact = () => {
+//   return(
+//     <div className="container dark">
+//       <Navbar/>
+//       <Footer/>
+//     </div>
+//   )
+// }
