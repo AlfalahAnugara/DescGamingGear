47f4fd50f5103f2a9182011086ee3e251e41fd80
AlfalahAnugara <alfalah190299@gmail.com>
2020-04-02 09:34:55 +0700

    Stateless component untuk menerima prop/data dari statefull component

--- src/stateless/ListHeadset.jsx
+++ src/stateless/ListHeadset.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from '../logo.svg';
+import React from 'react'
 
 const List = props => {
     return(
     <div className="col-md">
         <div class="card">
-            <img src={logo} class="card-img-top" style={{ height: "100px", width: "100px" }} alt="..." />
+            <img src={props.image} class="card-img-top" style={{ maxHeight: "200px", width: "auto" }}/>
             <div class="card-body">
                 <h5 class="card-title">{props.nama}</h5>
                 <p class="card-text">{props.ket}</p>
--- src/stateless/ListMouse.jsx
+++ src/stateless/ListMouse.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 
 const List = props => {
     return (
         <div className="col-md">
             <div class="card">
-                <img src={props.image} class="card-img-top" style={{ Height: "200px", width: "300px" }} />
+                <img src={props.image} class="card-img-top" style={{ Height: "300px", Width: "auto" }} />
                 <div class="card-body">
                     <h5 class="card-title">{props.nama}</h5>
                     <p class="card-text">{props.ket}</p>
